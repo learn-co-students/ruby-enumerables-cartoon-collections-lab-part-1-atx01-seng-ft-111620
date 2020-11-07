b512fb5bd2665cd9d9f5dfbdc7a1f229e4d549a9
Adri Torres <adriana.amaral@msn.com>
2020-11-07 02:57:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,25 +8,11 @@ array
 end
 
 
-# def list_dwarves(dwarves)
-
-# dwarves.each_with_index do |name, idx|
-# end
-# end 
-#   # Use `each_with_index` to enumerate over the provided array
-#   #
-#   # Print a numbered list of each element
-  
-  
 def list_dwarves(dwarves)
 dwarves.each_with_index {|name, index|  print " #{index +1}. #{name}" }
   
 end
-# hash = Hash.new
-# %w("Dopey", "Grumpy", "Bashful").each_with_index { |item, index|
-#   hash[item] = index
-# }
-# hash   
+ 
 
 
 
